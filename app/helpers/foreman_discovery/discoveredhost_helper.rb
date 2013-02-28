f5ced33c0dec559a5e33e50c97ba9e28995c156e
Greg Sutcliffe <gsutclif@redhat.com>
2013-02-28 14:53:12 +0000

    Add routes for facts and provisionin

--- app/helpers/foreman_discovery/discoveredhost_helper.rb
+++ app/helpers/foreman_discovery/discoveredhost_helper.rb
@@ -1,22 +1,22 @@
 module ForemanDiscovery
-module DiscoveredhostHelper
+  module DiscoveredhostHelper
 
-  def disc_report_column(record)
-    return record.last_report? ? time_ago_in_words(record.last_report.getlocal) +" ago": ""
+    def disc_report_column(record)
+      return record.last_report? ? time_ago_in_words(record.last_report.getlocal) +" ago": ""
+    end
+
+    def disc_host_title_actions(host)
+      title_actions(
+        button_group(
+          link_to("Refresh Facts", hash_for_refresh_facts_path(:id => host)),
+          link_to("Provision", hash_for_disc_convert_path(:id => host))
+      ),
+        button_group(
+          link_to_if_authorized("Delete", hash_for_host_path(:id => host, :auth_action => :destroy),
+                                +                              :class => "btn btn-danger", :confirm => 'Are you sure?', :method => :delete)
+      )
+      )
+    end
+
   end
-
-  def disc_host_title_actions(host)
-    title_actions(
-      button_group(
-        link_to("Refresh Facts", hash_for_refresh_facts_path(:id => host)),
-        link_to("Provision", hash_for_disc_convert_path(:id => host))
-    ),
-      button_group(
-        link_to_if_authorized("Delete", hash_for_host_path(:id => host, :auth_action => :destroy),
-                              +                              :class => "btn btn-danger", :confirm => 'Are you sure?', :method => :delete)
-    )
-    )
-  end
-
-end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@
 Rails.application.routes.draw do
 
   scope :module => "ForemanDiscovery" do
-    match 'discovered',     :to => 'discoveredhosts#discovered'
-    match 'discovered/:id', :to => 'discoveredhosts#disc_show', :as => 'disc'
+    match 'discovered'                   => 'discoveredhosts#discovered'
+    match 'discovered/:id'               => 'discoveredhosts#disc_show',     :as => 'disc'
+    match 'discovered/:id/convert'       => 'discoveredhosts#convert',       :as => 'disc_convert'
+    match 'discovered/:id/refresh_facts' => 'discoveredhosts#refresh_facts', :as => 'refresh_facts'
   end
 
 end
