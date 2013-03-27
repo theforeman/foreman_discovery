2f83960649e969452b4776374ba4972e8691609f
Greg Sutcliffe <gsutclif@redhat.com>
2013-03-27 13:52:39 +0000

    Only show discovered link to Admins

--- app/overrides/add_discovered_link.rb
+++ app/overrides/add_discovered_link.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "hosts/index", 
+Deface::Override.new(:virtual_path => "hosts/index",
                      :name => "add_discovered_link",
                      :insert_after => "code[erb-silent]:contains('title_actions')",
-                     :text => " <% title_actions link_to('Discovered', 'discovered/', :class => 'btn-primary') -%>")
+                     :text => " <% (title_actions link_to('Discovered', 'discovered/', :class => 'btn-primary')) if User.current.admin? -%>")
