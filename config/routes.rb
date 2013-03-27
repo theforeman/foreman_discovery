418d372c978f7c511169abdb583a6b6a06dcdf56
Joseph Mitchell Magen <jmagen@redhat.com>
2013-03-27 15:26:08 +0200

    fixes routing errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 # All routes entered here will automatically be pulled in Foreman
 Rails.application.routes.draw do
 
-  scope :module => "ForemanDiscovery" do
-    resources :discovered
-    match 'discovered/:id/refresh_facts' => 'discovered#refresh_facts', :as => 'refresh_facts'
-    match 'architecture_selected_discovered' => 'hosts#architecture_selected'
-    match 'os_selected_discovered' => 'hosts#os_selected'
-    match 'medium_selected_discovered' => 'hosts#medium_selected'
+  scope :module => "foreman_discovery" do
+    constraints(:id => /[^\/]+/) do
+      resources :discovered
+      match 'discovered/:id/refresh_facts' => 'discovered#refresh_facts', :as => 'refresh_facts'
+      match 'architecture_selected_discovered' => 'hosts#architecture_selected'
+      match 'os_selected_discovered' => 'hosts#os_selected'
+      match 'medium_selected_discovered' => 'hosts#medium_selected'
+    end
   end
 
 end
