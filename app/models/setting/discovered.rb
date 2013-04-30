d24c391544261b3ee956fb42f4cf14afaa9ecb39
Greg Sutcliffe <gsutclif@redhat.com>
2013-04-30 07:49:45 +0100

    Can't access Location.first (or Org) at this point in the initializers, hardcode for now

--- app/models/setting/discovered.rb
+++ app/models/setting/discovered.rb
@@ -7,14 +7,14 @@ class Setting::Discovered < ::Setting
     if SETTINGS[:locations_enabled]
       Setting.transaction do
         [
-          self.set('discovery_location', "The default Location to place Discovered Hosts in", Location.first.name ),
+          self.set('discovery_location', "The default Location to place Discovered Hosts in", "Default" ),
         ].compact.each { |s| self.create s.update(:category => "Setting::Discovered")}
       end
     end
     if SETTINGS[:organizations_enabled]
       Setting.transaction do
         [
-          self.set('discovery_organization', "The default Organization to place Discovered Hosts in", Organization.first.name ),
+          self.set('discovery_organization', "The default Organization to place Discovered Hosts in", "Default" ),
         ].compact.each { |s| self.create s.update(:category => "Setting::Discovered")}
       end
     end
