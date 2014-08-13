b4220a2ab1efba6ac33c8ebe5d630a3905692e81
Marek Hulan <mhulan@redhat.com>
2014-08-13 11:05:59 +0200

    Refs #6925 - Fix wrong IP address fact name

--- app/models/host/discovered.rb
+++ app/models/host/discovered.rb
@@ -30,7 +30,7 @@ class Host::Discovered < ::Host::Base
     fact_name = Setting[:discovery_fact] || 'macaddress'
     hostname = facts[fact_name].try(:downcase).try(:gsub,/:/,'').try(:sub,/^/,'mac')
     raise(::Foreman::Exception.new(N_("Invalid facts: hash does not contain the required fact '%s'"), fact_name)) unless hostname
-    raise(::Foreman::Exception.new(N_("Invalid facts: hash does not contain IP address"))) unless facts[:ip]
+    raise(::Foreman::Exception.new(N_("Invalid facts: hash does not contain IP address"))) unless facts['ipaddress']
 
     # filter facts
     facts.reject!{|k,v| k =~ /kernel|operatingsystem|osfamily|ruby|path|time|swap|free|filesystem/i }
