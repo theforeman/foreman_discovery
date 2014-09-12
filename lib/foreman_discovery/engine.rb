6bf4687fe8640751d413b6ae0b03d9216f2466b7
Marek Hulan <mhulan@redhat.com>
2014-09-12 15:58:31 +0200

    Fix #7435 - use discovery_bootif for primary interface detection
    Refs https://bugzilla.redhat.com/show_bug.cgi?id=1140334

--- lib/foreman_discovery/engine.rb
+++ lib/foreman_discovery/engine.rb
@@ -75,6 +75,7 @@ module ForemanDiscovery
     config.to_prepare do
       # Include host extensions
       ::Host::Managed.send :include, Host::ManagedExtensions
+      ::PuppetFactParser.send :include, PuppetFactParserExtensions
     end
 
     rake_tasks do
