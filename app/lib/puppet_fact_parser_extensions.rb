6d1ebc38563f23212be558bd73224ed84bd782ad
Stephen Benjamin <stbenjam@redhat.com>
2014-11-15 19:36:42 +0100

    fixes #8411 - check if interfaces fact exists

--- app/lib/puppet_fact_parser_extensions.rb
+++ app/lib/puppet_fact_parser_extensions.rb
@@ -17,9 +17,11 @@ module PuppetFactParserExtensions
 
   # search for IP of interface with primary interface macaddress (ipaddress fact does not have to be interface used for boot)
   def ip_with_discovery_fact
-    facts[:interfaces].split(',').each do |interface|
-      if facts["macaddress_#{interface}"].try(:downcase) == facts[discovery_mac_fact_name].try(:downcase)
-        return facts["ipaddress_#{interface}"]
+    if facts[:interfaces]
+      facts[:interfaces].split(',').each do |interface|
+        if facts["macaddress_#{interface}"].try(:downcase) == facts[discovery_mac_fact_name].try(:downcase)
+          return facts["ipaddress_#{interface}"]
+        end
       end
     end
     ip_without_discovery_fact # fallback if IP was not found
