067a64a8ce856dae85a2d5f20312c2a67d808433
Ori Rabin <orabin@redhat.com>
2015-01-12 10:51:36 +0200

    Fixes #8900 - disks_size is bigger to fix migration on pg

--- db/migrate/20141223101707_create_discovery_attribute_sets.rb
+++ db/migrate/20141223101707_create_discovery_attribute_sets.rb
@@ -2,10 +2,10 @@ class CreateDiscoveryAttributeSets < ActiveRecord::Migration
   def change
     create_table :discovery_attribute_sets do |t|
       t.references :host
-      t.integer :memory, :default => 0
+      t.integer :memory, :limit => 8, :default => 0
       t.integer :cpu_count, :default => 0
       t.integer :disk_count, :default => 0
-      t.integer :disks_size, :default => 0
+      t.integer :disks_size, :limit => 8, :default => 0
       t.timestamps
     end
     add_index :discovery_attribute_sets, :host_id
