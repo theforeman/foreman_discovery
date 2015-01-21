d16abaf0b7b3a9380fdb06c59671868957c09710
Lukas Zapletal <lzap+git@redhat.com>
2015-01-21 11:41:45 +0100

    Fixed version requirement in engine.rb

--- lib/foreman_discovery/engine.rb
+++ lib/foreman_discovery/engine.rb
@@ -36,7 +36,7 @@ module ForemanDiscovery
 
     initializer 'foreman_discovery.register_plugin', :after=> :finisher_hook do |app|
       Foreman::Plugin.register :foreman_discovery do
-        requires_foreman '>= 1.6'
+        requires_foreman '>= 1.7'
 
         # discovered hosts permissions
         security_block :discovery do
