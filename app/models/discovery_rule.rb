58bd39c8d62f06f513a1ec7c829a9ccb7556955c
Ori Rabin <orabin@redhat.com>
2015-01-26 10:06:31 +0200

    Fixes #9090 - In discovery_rule host limit and priority can not be negative numbers

--- app/models/discovery_rule.rb
+++ app/models/discovery_rule.rb
@@ -12,8 +12,8 @@ class DiscoveryRule < ActiveRecord::Base
   validates :hostname, :format => { :with => /\A[a-zA-Z<]/, :message => N_("must start with a letter or ERB.") },
     :allow_blank => true
   validates :hostgroup_id, :presence => true
-  validates :max_count, :numericality => { only_integer: true }
-  validates :priority, :presence => true, numericality: true
+  validates :max_count, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }
+  validates :priority, :presence => true, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }
   validates_lengths_from_database
 
   belongs_to :hostgroup
