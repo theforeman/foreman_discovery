d8a855ae4d2e968013df596e0ab95ad04035a05c
Eric D. Helms <ericdhelms@gmail.com>
2015-03-28 12:52:39 -0400

    Fixes #9912: Fix ambiguous column error by using table_name.

--- db/migrate/20150310153859_remove_discovery_attribute_sets_from_managed_hosts.rb
+++ db/migrate/20150310153859_remove_discovery_attribute_sets_from_managed_hosts.rb
@@ -1,6 +1,6 @@
 class RemoveDiscoveryAttributeSetsFromManagedHosts < ActiveRecord::Migration
   def up
-    DiscoveryAttributeSet.where(:id => DiscoveryAttributeSet.joins(:host).where(:'hosts.managed' => true).pluck(:id)).delete_all
+    DiscoveryAttributeSet.where(:id => DiscoveryAttributeSet.joins(:host).where(:'hosts.managed' => true).pluck("#{DiscoveryAttributeSet.table_name}.id")).delete_all
   end
 
   def down
