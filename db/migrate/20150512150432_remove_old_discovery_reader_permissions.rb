7bb7753292af71629baf9baa74eed5acf8de66af
Ori Rabin <orabin@redhat.com>
2015-05-19 10:47:34 +0300

    Refs #9895 - fixing resource type in discovery_rules permissions

--- db/migrate/20150512150432_remove_old_discovery_reader_permissions.rb
+++ db/migrate/20150512150432_remove_old_discovery_reader_permissions.rb
@@ -1,6 +1,8 @@
 class RemoveOldDiscoveryReaderPermissions < ActiveRecord::Migration
   def up
-    Permission.where("name like '%_discovery_rules' and resource_type is null").destroy_all
+    Permission.where("name like '%_discovery_rules' and resource_type is null").each do |permission|
+      permission.update_attributes(:resource_type => 'DiscoveryRule')
+    end
   end
 
   def down
