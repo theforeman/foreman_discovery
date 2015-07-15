2e664d97f1b9303ce2e42de802a5b71353fa25c4
Ori Rabin <orabin@redhat.com>
2015-07-15 09:40:16 +0300

    Refs #9963 - changing puts to say in permissions migration

--- db/migrate/20150331132115_remove_old_permissions.rb
+++ db/migrate/20150331132115_remove_old_permissions.rb
@@ -2,12 +2,12 @@ class RemoveOldPermissions < ActiveRecord::Migration
   def up
     # remove invalid permissions causing http://projects.theforeman.org/issues/9963
     perms = Permission.where("name like '%_discovered_hosts' and resource_type is null").destroy_all
-    puts "Removed invalid permissions: #{perms.inspect}" if perms.size > 0
+    say "Removed invalid permissions: #{perms.inspect}" if perms.size > 0
 
     # unassociate and remove unused role "Discovery" (renamed to "Discovery Manager")
     if old_role = Role.where(:name => "Discovery").first
       UserRole.where(:role_id => old_role.id).destroy_all
-      puts "Role 'Discovery' was removed, use 'Discovery Manager' instead" if old_role.destroy
+      say "Role 'Discovery' was removed, use 'Discovery Manager' instead" if old_role.destroy
     end
   end
 
