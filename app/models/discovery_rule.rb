5cb015eb6ab9be490956af00300bdd1df94c8d18
Lukas Zapletal <lzap+git@redhat.com>
2015-07-24 12:25:19 +0200

    Fixes #10469 - enforced discovery rule taxonomy

--- app/models/discovery_rule.rb
+++ app/models/discovery_rule.rb
@@ -15,6 +15,7 @@ class DiscoveryRule < ActiveRecord::Base
   validates :priority, :presence => true, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }
   validates_lengths_from_database
   before_validation :default_int_attributes
+  before_validation :enforce_taxonomy
 
   belongs_to :hostgroup
   has_many :hosts
@@ -36,5 +37,21 @@ class DiscoveryRule < ActiveRecord::Base
   def default_int_attributes
    self.max_count ||= 0
    self.priority  ||= 0
+  end
+
+  def enforce_taxonomy
+    return if hostgroup.nil?
+    if SETTINGS[:organizations_enabled]
+      unless (ms = hostgroup.organizations - organizations).empty?
+        names = ms.collect(&:name).to_sentence
+        errors.add(:organizations, n_("Host group organization %s must also be associated to the discovery rule", "Host group organizations %s must also be associated to the discovery rule", ms.size) % names)
+      end
+    end
+    if SETTINGS[:locations_enabled]
+      unless (ms = hostgroup.locations - locations).empty?
+        names = ms.collect(&:name).to_sentence
+        errors.add(:locations, n_("Host group location %s must also be associated to the discovery rule", "Host group locations %s must also be associated to the discovery rule", ms.size) % names)
+      end
+    end
   end
 end
