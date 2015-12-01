f8815b7039f30da7edb6a979b6383bd4701d7970
Lukas Zapletal <lzap+git@redhat.com>
2015-12-01 13:31:15 +0100

    Refs #12362 - fixed power API file separation

--- app/services/foreman_discovery/node_api/power.rb
+++ app/services/foreman_discovery/node_api/power.rb
@@ -2,15 +2,15 @@ module ForemanDiscovery::NodeAPI
   class Power
     class << self
       def service(data)
-        PowerService.new(data)
+        ForemanDiscovery::NodeAPI::PowerService.new(data)
       end
 
       def legacy_direct_service(data)
-        PowerLegacyDirectService.new(data)
+        ForemanDiscovery::NodeAPI::PowerLegacyDirectService.new(data)
       end
 
       def legacy_proxied_service(data)
-        PowerLegacyProxiedService.new(data)
+        ForemanDiscovery::NodeAPI::PowerLegacyProxiedService.new(data)
       end
     end
   end
