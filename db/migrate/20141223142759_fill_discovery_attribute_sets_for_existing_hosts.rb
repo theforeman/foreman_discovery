5f53eaf9f6d0edc18ecf199df293c3a18ca4ab98
Lukas Zapletal <lzap+git@redhat.com>
2016-04-14 16:02:42 +0200

    Fixes #14643 - fixed FillDiscoveryAttributeSetsForExistingHosts migration

--- db/migrate/20141223142759_fill_discovery_attribute_sets_for_existing_hosts.rb
+++ db/migrate/20141223142759_fill_discovery_attribute_sets_for_existing_hosts.rb
@@ -1,6 +1,20 @@
+class FakeDiscoveredHost < ActiveRecord::Base
+  self.table_name = 'hosts'
+end
+
 class FillDiscoveryAttributeSetsForExistingHosts < ActiveRecord::Migration
   def up
-    Host::Discovered.all.each { |host| host.populate_fields_from_facts}
+    FakeDiscoveredHost.where(:type => "Host::Discovered").all.each do |host|
+      begin
+        say "Populating attribute set for discovered host #{host.name}"
+        host.discovery_attribute_set = DiscoveryAttributeSet.where(:host_id => host.id).first_or_create
+        host.discovery_attribute_set.update_attributes(host.import_from_facts)
+        host.save!
+      rescue Exception => e
+        say "Error while populating host #{host.name}, deleting: #{e.message}:\n" + e.backtrace.join("\n")
+        host.destroy
+      end
+    end
   end
 
   def down
