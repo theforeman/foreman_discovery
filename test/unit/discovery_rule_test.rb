a3d413641ad5955dbf651efbf3846ed1d81b80d6
Swapnil Abnave <sabnave@redhat.com>
2016-09-27 13:24:13 -0400

    Fixes #16657 - max char limit 255 - discovery_rule columns
    Fixes 16657 - max char limit 255 - discovery_rule columns
    
    Fixes #16657 - max char limit 255 - discovery_rule columns

--- test/unit/discovery_rule_test.rb
+++ test/unit/discovery_rule_test.rb
@@ -9,6 +9,57 @@ class DiscoveryRuleTest < ActiveSupport::TestCase
     @hostgroup.save!
   end
 
+  context "#validates attribute" do
+    setup do
+      @huge_name = "x" * 256
+      @rule = DiscoveryRule.new :name => "myrule",
+                               :search => "cpu_count > 1",
+                               :priority => "1",
+                               :hostgroup_id => @hostgroup.id,
+                               :organization_ids => [@hg_org.id]
+    end
+
+    context "name" do
+      test "max length is 255 characters" do
+        @rule.name = @huge_name
+        assert @rule.invalid?
+        assert_equal "is too long (maximum is 255 characters)", @rule.errors[:name].first
+      end
+
+      test "can't have whitespace(s)" do
+        @rule.name = "my rule"
+        assert @rule.invalid?
+        assert_equal "can't contain white spaces.", @rule.errors[:name].first
+      end
+    end
+
+    context "search" do
+      test "max length is 255 characters" do
+        @rule.search = @huge_name
+        assert @rule.invalid?
+        assert_equal "is too long (maximum is 255 characters)", @rule.errors[:search].first
+      end
+    end
+
+    context "hostname" do
+      test "max length is 255 characters" do
+        @rule.hostname = "abc" +  @huge_name
+        assert @rule.invalid?
+        assert_equal "is too long (maximum is 255 characters)", @rule.errors[:hostname].first
+      end
+
+      test "must start with a letter or ERB" do
+        @rule.hostname = "123"
+        assert @rule.invalid?
+        assert_equal "must start with a letter or ERB.", @rule.errors[:hostname].first
+
+        @rule.hostname = "<%= host.hostname %>"
+        assert @rule.valid?
+        assert @rule.errors.blank?
+      end
+    end
+  end
+
   test "should be able to create a rule without entering hosts limit" do
     rule = DiscoveryRule.new :name => "myrule",
       :search => "cpu_count > 1",
