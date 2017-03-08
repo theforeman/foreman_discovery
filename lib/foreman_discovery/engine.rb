48ec0e18878add843493c4e08f53c2d7bf64f518
Marek Hulan <mhulan@redhat.com>
2017-03-08 10:53:09 +0100

    Fixes #18834 - unify label help with 1.15

--- lib/foreman_discovery/engine.rb
+++ lib/foreman_discovery/engine.rb
@@ -42,7 +42,7 @@ module ForemanDiscovery
 
     initializer 'foreman_discovery.register_plugin', :before => :finisher_hook do |app|
       Foreman::Plugin.register :foreman_discovery do
-        requires_foreman '>= 1.14.0'
+        requires_foreman '>= 1.15.0'
 
         # discovered hosts permissions
         security_block :discovery do
