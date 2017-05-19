e7b6506b74e6c088e23a827c9bcae56fc5fac63c
Lukas Zapletal <lzap+git@redhat.com>
2017-05-19 13:26:45 +0200

    Fixes #19313 - NIC associations are preserved
    During provisioning, NIC associations were cleared. This was introduced
    in e9c626c. I can no longer reproduce the problem it fixed and this is
    causing TFTP orchestration to be skipped due missing NIC associations.

--- app/services/foreman_discovery/host_converter.rb
+++ app/services/foreman_discovery/host_converter.rb
@@ -5,9 +5,7 @@ class ForemanDiscovery::HostConverter
   # Creates shallow copy.
   def self.to_managed(original_host, set_managed = true, set_build = true, added_attributes = {})
     host = original_host.becomes(::Host::Managed)
-    host.clear_association_cache
     host.type = 'Host::Managed'
-
     host.attributes = host.apply_inherited_attributes(added_attributes)
     host.set_hostgroup_defaults if host.hostgroup_id.present?
 
