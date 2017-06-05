bfdd67eda119a43abf60f0e5edc8ac703d18bc3c
Eric D. Helms <ericdhelms@gmail.com>
2017-06-05 13:46:04 -0400

    Fixes #19944 - Move default role permissions to seeds

---
+++ db/seeds.d/90_add_permissions_from_default_roles.rb
@@ -0,0 +1,10 @@
+default_permissions = Foreman::Plugin.find("foreman_discovery").default_roles
+
+["Discovery Reader", "Discovery Manager"].each do |role_name|
+  role = Role.find_by_name(role_name) || next
+  default_permissions[role_name].each do |permission|
+    role.add_permissions!(permission) unless role.permission_names.include?(permission.to_sym)
+  end
+  role.update_attributes :origin => "discovery", :description => "Discovery plugin built-in role"
+  role.save!
+end
