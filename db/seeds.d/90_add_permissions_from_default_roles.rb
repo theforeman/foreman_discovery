24ec75a9c30c41780e71d4e9266141cdd423b91e
Lukas Zapletal <lzap+git@redhat.com>
2017-08-11 11:44:42 +0200

    Fixes #20564 - ignore locking in migration

--- db/seeds.d/90_add_permissions_from_default_roles.rb
+++ db/seeds.d/90_add_permissions_from_default_roles.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
 default_permissions = Foreman::Plugin.find("foreman_discovery").default_roles
 
 ["Discovery Reader", "Discovery Manager"].each do |role_name|
-  role = Role.find_by_name(role_name) || next
-  default_permissions[role_name].each do |permission|
-    role.add_permissions!(permission) unless role.permission_names.include?(permission.to_sym)
+  Role.ignore_locking do
+    role = Role.unscoped.find_by_name(role_name) || next
+    default_permissions[role_name].each do |permission|
+      role.add_permissions(permission) unless role.permission_names.include?(permission.to_sym)
+    end
+    role.origin = 'discovery'
+    role.description = 'Discovery plugin built-in role'
+    role.save
   end
-  role.ignore_locking do |r|
-    r.update_attributes :origin => "discovery", :description => "Discovery plugin built-in role"
-  end
-  role.save!
 end
