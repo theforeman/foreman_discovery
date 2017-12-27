4fedd4488047d3ade8e8bebc327944deb19c4aae
Timo Goebel <timo.goebel@dm.de>
2017-12-27 14:12:19 +0100

    fixes #22083 - replace deprecated am dirty methods

--- app/models/nic/managed_extensions.rb
+++ app/models/nic/managed_extensions.rb
@@ -8,7 +8,7 @@ module Nic::ManagedExtensions
   def discovery_queue_rebuild_dns
     return unless (dns? || dns6? || reverse_dns? || reverse_dns6?) && errors.empty? && Setting[:discovery_always_rebuild_dns]
     return if self.host.new_record? || old.nil? # Discovered Hosts already exist, and new_records will break `find`
-    return unless self.host.type_changed? and ::Host::Base.find(self.host.id).type == "Host::Discovered"
+    return unless self.host.will_save_change_to_attribute?(:type, from: 'Host::Discovered')
     return if self.pending_dns_record_changes?
     logger.debug "Queuing DNS rebuild for #{self}"
     queue.create(:name => _("Rebuild DNS for %s") % self, :priority => 10, :action => [self, :set_discovery_rebuild_dns])
