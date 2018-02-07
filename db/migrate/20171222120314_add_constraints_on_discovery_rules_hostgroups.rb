6efc6ccf4cd7c2275d00d74e30b39678cc15ac63
Lukas Zapletal <lzap+git@redhat.com>
2018-02-07 12:32:57 +0100

    Refs #21941 - rules foreign key rollback works

--- db/migrate/20171222120314_add_constraints_on_discovery_rules_hostgroups.rb
+++ db/migrate/20171222120314_add_constraints_on_discovery_rules_hostgroups.rb
@@ -1,6 +1,10 @@
  class AddConstraintsOnDiscoveryRulesHostgroups < ActiveRecord::Migration[4.2]
   def change
-    DiscoveryRule.unscoped.where("hostgroup_id IS NULL OR hostgroup_id NOT IN (?)", Hostgroup.unscoped.all.pluck(:id)).delete_all
+    reversible do |dir|
+      dir.up do
+        DiscoveryRule.unscoped.where("hostgroup_id IS NULL OR hostgroup_id NOT IN (?)", Hostgroup.unscoped.all.pluck(:id)).delete_all
+      end
+    end
     add_foreign_key "discovery_rules", "hostgroups", name: "discovery_rules_hostgroup_id_fk", :column => "hostgroup_id"
   end
  end
