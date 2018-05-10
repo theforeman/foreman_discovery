25ee65b7041815cabeee5785aeab21e17c36908c
Ido Kanner <ikanner@redhat.com>
2018-05-10 11:31:03 +0300

    Fixes #23554 - Handle both cases of options[:user] as arrive from foreman

--- app/mailers/discovered_mailer.rb
+++ app/mailers/discovered_mailer.rb
@@ -1,7 +1,18 @@
 class DiscoveredMailer < ::ApplicationMailer
   helper :discovered_hosts
   def discovered_summary(options = {})
-    raise ::Foreman::Exception.new(N_("Must specify a user with email enabled")) unless (user = User.find(options[:user])) && user.mail_enabled?
+    user = if options[:user].kind_of? User
+             options[:user]
+           elsif options[:user].kind_of? Integer
+             User.find(options[:user])
+           else
+             raise ::Foreman::Exception.new(N_('Invalid user type of %s was provided'), options[:user].class.to_s)
+           end
+
+    unless user.mail_enabled?
+      Rails.logger.debug("The user #{user.id} does not email enabled")
+      return
+    end
     begin
       Time.zone = user.timezone
     rescue ArgumentError
