14bff9f73f9f3aec422a194983df6530554564d7
odovzhenko <oleg.dovzhenko@gmail.com>
2018-06-04 12:48:00 +0300

    Fixes #23793 - Port robottelo tests for settings

---
+++ test/functional/api/v2/settings_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_plugin_helper'
+
+class Api::V2::SettingsControllerTest < ActionController::TestCase
+  setup do
+    FactoryBot.create(:setting, :name => 'discovery_prefix', :value => 'mac', :category => 'Setting::Discovered')
+  end
+
+  test_attributes :pid => '2c5ecb7e-87bc-4980-9620-7ae00e3f360e'
+  test "should update hostname prefix without value" do
+    setting = Setting.find_by_name("discovery_prefix")
+    put :update, params: { :id => setting.id, :setting => { :value => '' } }
+    assert_equal JSON.parse(@response.body)['value'], '', "Can't update discovery_prefix setting with empty value"
+  end
+
+  test_attributes :pid => '4969994d-f934-4f0e-9a98-476b87eb0527'
+  test "should update hostname prefix" do
+    value = RFauxFactory.gen_alpha
+    setting = Setting.find_by_name("discovery_prefix")
+    put :update, params: { :id => setting.id, :setting => { :value => value } }
+    assert_equal JSON.parse(@response.body)['value'], value, "Can't update discovery_prefix setting with valid value #{value}"
+  end
+end
