5eb39142ff33c37eb3f6ddb19c7e30b615b1d840
Lukas Zapletal <lzap+git@redhat.com>
2019-07-18 17:34:37 +0200

    Refs #27340 - fixed broken unit tests

--- test/unit/discovery_attribute_set_test.rb
+++ test/unit/discovery_attribute_set_test.rb
@@ -18,34 +18,38 @@ class DiscoveryAttributeSetTest < ActiveSupport::TestCase
 
   test "can search discovered hosts by cpu" do
     host = discover_host_from_facts(@facts)
-    results = Host::Discovered.search_for("cpu_count = #{host.facts_hash['physicalprocessorcount'].to_i}")
+    results = Host::Discovered.unscoped.search_for("cpu_count = #{host.facts_hash['physicalprocessorcount'].to_i}")
     assert_equal 1, results.count
-    results = Host::Discovered.search_for("cpu_count > #{host.facts_hash['physicalprocessorcount'].to_i}")
+    results = Host::Discovered.unscoped.search_for("cpu_count > #{host.facts_hash['physicalprocessorcount'].to_i}")
     assert_equal 0, results.count
   end
 
   test "can search discovered hosts by memory" do
     host = discover_host_from_facts(@facts)
-    results = Host::Discovered.search_for("memory = #{host.facts_hash['memorysize_mb'].to_f.ceil}")
+    results = Host::Discovered.unscoped.search_for("memory = #{host.facts_hash['memorysize_mb'].to_f.ceil}")
     assert_equal 1, results.count
-    results = Host::Discovered.search_for("memory > #{host.facts_hash['memorysize_mb'].to_f.ceil}")
+    results = Host::Discovered.unscoped.search_for("memory > #{host.facts_hash['memorysize_mb'].to_f.ceil}")
     assert_equal 0, results.count
   end
 
   test "can search discovered hosts by disk_count" do
     host = discover_host_from_facts(@facts)
-    results = Host::Discovered.search_for("disk_count = 1")
+    results = Host::Discovered.unscoped.search_for("disk_count = 1")
     assert_equal 1, results.count
-    results = Host::Discovered.search_for("disk_count = 3")
+    results = Host::Discovered.unscoped.search_for("disk_count = 3")
     assert_equal 0, results.count
   end
 
   test "can search discovered hosts by disks_size" do
+    excluded = Setting[:excluded_facts]
+    Setting[:excluded_facts] = ["test"]
     host = discover_host_from_facts(@facts)
     disks_size = (host.facts_hash['blockdevice_sda_size'].to_f / 1024 / 1024).ceil
-    results = Host::Discovered.search_for("disks_size = #{disks_size}")
+    results = Host::Discovered.unscoped.search_for("disks_size = #{disks_size}")
     assert_equal 1, results.count
-    results = Host::Discovered.search_for("disks_size > #{disks_size}")
+    results = Host::Discovered.unscoped.search_for("disks_size > #{disks_size}")
     assert_equal 0, results.count
+  ensure
+    Setting[:excluded_facts] = excluded
   end
 end
