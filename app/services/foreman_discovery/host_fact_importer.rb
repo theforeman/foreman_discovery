a129c0ed292692fb7ca4dd24ef308d18443ab719
Lukáš Zapletal <lzap+git@redhat.com>
2020-08-10 09:31:34 +0200

    Update app/services/foreman_discovery/host_fact_importer.rb
    Co-authored-by: Ondřej Ezr <ezrik12@gmail.com>

--- app/services/foreman_discovery/host_fact_importer.rb
+++ app/services/foreman_discovery/host_fact_importer.rb
@@ -2,7 +2,7 @@ class ForemanDiscovery::HostFactImporter < ::HostFactImporter
     def import_facts(facts)
         # Discovered Hosts won't report in via puppet, so we can use that field to
         # record the last time it sent facts...
-        self.last_report = Time.now
+        host.last_report = Time.now
         # Set the correct facts type for new foreman facts importing code.
         facts[:_type] = :foreman_discovery
         super(facts)
