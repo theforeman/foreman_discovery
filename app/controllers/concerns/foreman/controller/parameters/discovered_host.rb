a50f44844cc9c63be9f59a27d6c92a3a33625003
Ondřej Ezr <oezr@redhat.com>
2021-10-21 11:46:28 +0200

    Add Puppet params with puppet plugin
    (cherry picked from commit 78fcd33e0199b6934ee096ced0841aae1657303d)

--- app/controllers/concerns/foreman/controller/parameters/discovered_host.rb
+++ app/controllers/concerns/foreman/controller/parameters/discovered_host.rb
@@ -1,6 +1,9 @@
 module Foreman::Controller::Parameters::DiscoveredHost
   extend ActiveSupport::Concern
   include Foreman::Controller::Parameters::Host
+  if defined?(ForemanPuppet)
+    include ForemanPuppet::Extensions::ParametersHost
+  end
 
   class_methods do
     def discovered_host_params_filter
@@ -9,23 +12,32 @@ module Foreman::Controller::Parameters::DiscoveredHost
 
         add_host_base_params_filter(filter)
         add_host_common_params_filter(filter)
+        add_host_puppet_params_filter(filter) if defined?(ForemanPuppet)
       end
     end
   end
 
   def discovered_host_params
-    self.class.discovered_host_params_filter.filter_params(params, parameter_filter_context)
+    filtered_params = self.class.discovered_host_params_filter.filter_params(params, parameter_filter_context)
+    process_deprecated_puppet_params!(filtered_params) if defined?(ForemanPuppet)
+    filtered_params
   end
 
   def discovered_host_params_host
-    self.class.discovered_host_params_filter.filter_params(params, parameter_filter_context, :host)
+    filtered_params = self.class.discovered_host_params_filter.filter_params(params, parameter_filter_context, :host)
+    process_deprecated_puppet_params!(filtered_params) if defined?(ForemanPuppet)
+    filtered_params
   end
 
   def managed_host_params_host
-    self.class.host_params_filter.filter_params(params, parameter_filter_context, :host)
+    filtered_params = self.class.host_params_filter.filter_params(params, parameter_filter_context, :host)
+    process_deprecated_puppet_params!(filtered_params) if defined?(ForemanPuppet)
+    filtered_params
   end
 
   def managed_host_params
-    self.class.host_params_filter.filter_params(params, parameter_filter_context)
+    filtered_params = self.class.host_params_filter.filter_params(params, parameter_filter_context)
+    process_deprecated_puppet_params!(filtered_params) if defined?(ForemanPuppet)
+    filtered_params
   end
 end
