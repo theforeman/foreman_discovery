be83a1a18d54c9d2b4f8144d6cebafb5e844e072
Lukas Zapletal <lzap+git@redhat.com>
2021-11-15 14:35:27 +0100

    Fixes #33917 - skip invalid IP address

--- app/services/foreman_discovery/subnet_suggestion.rb
+++ app/services/foreman_discovery/subnet_suggestion.rb
@@ -14,7 +14,14 @@ module ForemanDiscovery
     def call
       return unless ip
 
-      subnet = Subnet.unscoped.subnet_for(ip)
+      begin
+        subnet = Subnet.unscoped.subnet_for(ip)
+      rescue Exception => e
+        Rails.logger.error "Error while detecting subnet for IP #{ip}"
+        Foreman::Logging.exception "Unable to detect subnet", e
+        subnet = nil
+      end
+
       if subnet
         Rails.logger.info "Detected #{kind} subnet: #{subnet} with taxonomy #{subnet.organizations.collect(&:name)}/#{subnet.locations.collect(&:name)}"
       else
