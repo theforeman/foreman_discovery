910f795e3939816597993c07539c877f5d4349cf
Lukas Zapletal <lzap+git@redhat.com>
2021-11-25 15:26:15 +0100

    Revert "Fixes #33998 - EnsureNotUsedBy has no namespce"
    This reverts commit 8a7479a3087fedfe3d15be2c98c685498e847f05.
    
    Not compatible with Foreman 3.1.

--- app/models/concerns/discovery_taxonomy_extensions.rb
+++ app/models/concerns/discovery_taxonomy_extensions.rb
@@ -2,6 +2,6 @@ module DiscoveryTaxonomyExtensions
   extend ActiveSupport::Concern
   included do
     has_many :discovered_hosts, :class_name => 'Host::Discovered'
-    before_destroy EnsureNotUsedBy.new(:discovered_hosts)
+    before_destroy ActiveRecord::Base::EnsureNotUsedBy.new(:discovered_hosts)
   end
 end
