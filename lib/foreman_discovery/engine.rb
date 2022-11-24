theforeman/foreman_discovery

commit 68ed667859ad01881975f14c1b12f04f2aa183ea

Fixes #33699 - restore compatibility with vanilla Foreman

lib/foreman_discovery/engine.rb
@@ -126,14 +126,12 @@ module ForemanDiscovery
           :assign_locations,
           :view_architectures,
           :view_domains,
-          :view_environments,
           :view_hostgroups,
           :view_media,
           :view_models,
           :view_operatingsystems,
           :view_provisioning_templates,
           :view_ptables,
-          :view_puppetclasses,
           :view_realms,
           :view_smart_proxies,
           :view_subnets,
@@ -149,6 +147,9 @@ module ForemanDiscovery
           :edit_discovery_rules,
           :destroy_discovery_rules,
         ]
+        if defined?(ForemanPuppet::VERSION) 
+          MANAGER += [ :view_environments, :view_puppetclasses ]
+        end
         role "Discovery Reader", READER, "Role granting permissions to view discovered hosts"
         role "Discovery Manager", MANAGER, "Role granting permissions to perform provisioning of discovered hosts"
 

commit 23b2b88f537adc3b5a28c14f2c59e6344df6cdb5

Avoid re-declaring of a constant
This was generating a warning, possibly because of constants being
frozen.

Fixes: 68ed667859ad01881975f14c1b12f04f2aa183ea

lib/foreman_discovery/engine.rb
@@ -309,10 +309,7 @@ module ForemanDiscovery
           :execute_discovery_rules,
           :edit_discovery_rules,
           :destroy_discovery_rules,
-        ]
-        if defined?(ForemanPuppet::Engine)
-          MANAGER += [ :view_environments, :view_puppetclasses ]
-        end
+        ] + (defined?(ForemanPuppet::Engine) ? [ :view_environments, :view_puppetclasses ] : [])
         role "Discovery Reader", READER, "Role granting permissions to view discovered hosts"
         role "Discovery Manager", MANAGER, "Role granting permissions to perform provisioning of discovered hosts"
 