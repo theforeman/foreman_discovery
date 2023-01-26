9b52424c51b00c21b3a3457faea19c6292e38b34
Leos Stejskal <lstejska@redhat.com>
2023-01-26 14:25:08 +0100

    Fixes #36006 - Migration error 'column settings.category does not exist'

--- db/migrate/20221102065954_fix_discovery_settings_category_to_dsl.rb
+++ db/migrate/20221102065954_fix_discovery_settings_category_to_dsl.rb
@@ -3,7 +3,7 @@
 class FixDiscoverySettingsCategoryToDsl < ActiveRecord::Migration[6.0]
   def up
     # rubocop:disable Rails/SkipsModelValidations
-    Setting.where(category: 'Setting::Discovered').update_all(category: 'Setting')
+    Setting.where(category: 'Setting::Discovered').update_all(category: 'Setting') if column_exists?(:settings, :category)
     # rubocop:enable Rails/SkipsModelValidations
   end
 end
