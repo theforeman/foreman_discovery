af09e3532e02ab5f55026e5eb62602e1f711be4b
archanaserver <ak31960466@gmail.com>
2024-01-16 17:10:55 +0530

    Fix Rails/FindEach cop

--- db/migrate/20141223142759_fill_discovery_attribute_sets_for_existing_hosts.rb
+++ db/migrate/20141223142759_fill_discovery_attribute_sets_for_existing_hosts.rb
@@ -4,7 +4,7 @@ end
 
 class FillDiscoveryAttributeSetsForExistingHosts < ActiveRecord::Migration[4.2]
   def up
-    FakeDiscoveredHost.where(:type => "Host::Discovered").all.each do |host|
+    FakeDiscoveredHost.where(:type => "Host::Discovered").all.find_each do |host|
       begin
         say "Populating attribute set for discovered host #{host.name}"
         ForemanDiscovery::ImportHooks::DiscoveryAttribute.new(host: host, facts: host.facts_hash).after_populate
