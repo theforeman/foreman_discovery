4bf7617dbf53b7b68cf9a263e4fca15528a0e551
Leos Stejskal <lstejska@redhat.com>
2025-01-21 10:44:48 +0100

    Regenerate Rubocop's todo & make it happy

--- app/controllers/concerns/foreman/controller/parameters/discovered_host.rb
+++ app/controllers/concerns/foreman/controller/parameters/discovered_host.rb
@@ -1,43 +1,49 @@
-module Foreman::Controller::Parameters::DiscoveredHost
-  extend ActiveSupport::Concern
-  include Foreman::Controller::Parameters::Host
-  if defined?(ForemanPuppet)
-    include ForemanPuppet::Extensions::ParametersHost
-  end
+module Foreman
+  module Controller
+    module Parameters
+      module DiscoveredHost
+        extend ActiveSupport::Concern
+        include Foreman::Controller::Parameters::Host
+        if defined?(ForemanPuppet)
+          include ForemanPuppet::Extensions::ParametersHost
+        end
 
-  class_methods do
-    def discovered_host_params_filter
-      Foreman::ParameterFilter.new(::Host::Discovered).tap do |filter|
-        filter.permit :discovery_rule_id
+        class_methods do
+          def discovered_host_params_filter
+            Foreman::ParameterFilter.new(::Host::Discovered).tap do |filter|
+              filter.permit :discovery_rule_id
 
-        add_host_base_params_filter(filter)
-        add_host_common_params_filter(filter)
-        add_host_puppet_params_filter(filter) if defined?(ForemanPuppet)
+              add_host_base_params_filter(filter)
+              add_host_common_params_filter(filter)
+              add_host_puppet_params_filter(filter) if defined?(ForemanPuppet)
+            end
+          end
+        end
+
+        def discovered_host_params
+          filtered_params = self.class.discovered_host_params_filter.filter_params(params, parameter_filter_context)
+          process_deprecated_puppet_params!(filtered_params) if defined?(ForemanPuppet)
+          filtered_params
+        end
+
+        def discovered_host_params_host
+          filtered_params = self.class.discovered_host_params_filter.filter_params(params, parameter_filter_context, :host)
+          process_deprecated_puppet_params!(filtered_params) if defined?(ForemanPuppet)
+          filtered_params
+        end
+
+        def managed_host_params_host
+          filtered_params = self.class.host_params_filter.filter_params(params, parameter_filter_context, :host)
+          process_deprecated_puppet_params!(filtered_params) if defined?(ForemanPuppet)
+          filtered_params
+        end
+
+        def managed_host_params
+          filtered_params = self.class.host_params_filter.filter_params(params, parameter_filter_context)
+          process_deprecated_puppet_params!(filtered_params) if defined?(ForemanPuppet)
+          filtered_params
+        end
       end
     end
-  end
-
-  def discovered_host_params
-    filtered_params = self.class.discovered_host_params_filter.filter_params(params, parameter_filter_context)
-    process_deprecated_puppet_params!(filtered_params) if defined?(ForemanPuppet)
-    filtered_params
-  end
-
-  def discovered_host_params_host
-    filtered_params = self.class.discovered_host_params_filter.filter_params(params, parameter_filter_context, :host)
-    process_deprecated_puppet_params!(filtered_params) if defined?(ForemanPuppet)
-    filtered_params
-  end
-
-  def managed_host_params_host
-    filtered_params = self.class.host_params_filter.filter_params(params, parameter_filter_context, :host)
-    process_deprecated_puppet_params!(filtered_params) if defined?(ForemanPuppet)
-    filtered_params
-  end
-
-  def managed_host_params
-    filtered_params = self.class.host_params_filter.filter_params(params, parameter_filter_context)
-    process_deprecated_puppet_params!(filtered_params) if defined?(ForemanPuppet)
-    filtered_params
   end
 end
--- app/controllers/concerns/foreman/controller/parameters/discovery_rule.rb
+++ app/controllers/concerns/foreman/controller/parameters/discovery_rule.rb
@@ -1,17 +1,23 @@
-module Foreman::Controller::Parameters::DiscoveryRule
-  extend ActiveSupport::Concern
-  include Foreman::Controller::Parameters::Taxonomix
+module Foreman
+  module Controller
+    module Parameters
+      module DiscoveryRule
+        extend ActiveSupport::Concern
+        include Foreman::Controller::Parameters::Taxonomix
 
-  class_methods do
-    def discovery_rule_params_filter
-      Foreman::ParameterFilter.new(::DiscoveryRule).tap do |filter|
-        filter.permit :name, :search, :hostname, :priority, :enabled, :hostgroup, :hostgroup_id, :max_count
-        add_taxonomix_params_filter(filter)
+        class_methods do
+          def discovery_rule_params_filter
+            Foreman::ParameterFilter.new(::DiscoveryRule).tap do |filter|
+              filter.permit :name, :search, :hostname, :priority, :enabled, :hostgroup, :hostgroup_id, :max_count
+              add_taxonomix_params_filter(filter)
+            end
+          end
+        end
+
+        def discovery_rule_params
+          self.class.discovery_rule_params_filter.filter_params(params, parameter_filter_context)
+        end
       end
     end
-  end
-
-  def discovery_rule_params
-    self.class.discovery_rule_params_filter.filter_params(params, parameter_filter_context)
   end
 end
